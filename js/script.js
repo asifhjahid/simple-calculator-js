6f5ed8a421acbf02ef76221fcf7506172e294568
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-05 03:35:02 +0600

    operator

--- js/script.js
+++ js/script.js
@@ -1,25 +1,71 @@
+function getInput(){
+    let inputValue = document.getElementById('inputValue').value;
+    return inputValue;
+    
+}
 
 function getInputValue(num){
-    let inputValue = document.getElementById('inputValue').value =num;
-    console.log(inputValue);
+    document.getElementById('inputValue').value =num;
+    
 }
-getInputValue(123); 
 
-function getResult(num){
-    let inputValue = document.getElementById('result').value =num;
-    console.log(inputValue);
+function getOutput(){
+    let inputResult = document.getElementById('result').value;
+    return inputResult;
+    
 }
-getResult(456); 
+
+function getOutputResult(num){
+     document.getElementById('result').value =num;
+    
+}
 
 
 
 let digits = document.querySelectorAll('.digit');
-for(let i=1; i<digits.length;i++){
+for(let i=0; i<digits.length;i++){
     let digit = digits[i];
-    // console.log(digit);
     digit.addEventListener('click',function(){
-        // console.log(this);
-        console.log('digit clicked',this.id);
+        
+        let inputDigit = getInput();
+        inputDigit = inputDigit + this.innerText;
+        getInputValue(inputDigit)
+        // let test = document.getElementById('inputValue').value;
+        // test = test = this.innerText;
+        // getInputValue(test)
+        if(this.id =="clear"){
+            getInputValue('')
+            getOutputResult('')
+        }
+        else if(this.id == 'backspace'){
+            var inputNumber = getInput();
+            console.log(inputNumber)
+            // let inputNumber = parseFloat(inputDigit)
+            inputNumber = inputNumber.slice(0, -1);
+            console.log(inputNumber);
+            getInputValue(inputNumber);
+            getOutputResult('')
+        }
+        
+        
         
     })
 }
+
+
+// let operators = document.querySelectorAll('.operator');
+// for(let i=0; i<operators.length;i++){
+//     let operator = operators[i];
+//     operator.addEventListener('click',function(){
+        
+//        if(this.id =="clear"){
+//             getInputValue('')
+//             getOutputResult('')
+//         }
+        
+        
+        
+        
+//     })
+// }
+
